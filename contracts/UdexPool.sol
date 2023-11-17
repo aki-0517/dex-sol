da81d2fabc4ef097bc343d0de2e087fcd9815644
aki-0517 <akki.pp.777@gmail.com>
2023-11-17 22:08:38 +0900

    add factory

---
+++ contracts/UdexPool.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract UdexPool {
+    address immutable public factory;
+    address public token0;
+    address public token1;
+
+    constructor() {
+        factory = msg.sender;
+    }
+
+    function initialize(address _token0, address _token1) external {
+        require(msg.sender == factory, 'UdexPool: INITIALIZATION_FORBIDDEN');
+        token0 = _token0;
+        token1 = _token1;
+    }
+
+}
---
+++ test/UdexFactory.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import type { ContractFactory, Contract } from "ethers";
+
+const TEST_ADDRESSES: [string, string] = [
+    '0x1230000000000000000000000000000000000000',
+    '0x3456000000000000000000000000000000000000'    
+]
